441bd11600a73e1b7de6f1e802097c1aff3636a0
manalijpatil <manalipatil2508@gmail.com>
2024-11-22 18:33:12 +0530

    Add project files.

---
+++ ConsoleApp1/Bank.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Bank
+    {
+        //Create a Bank a class, with Debit & Credit method
+        //You can assign a default balance using constructor
+        //Credit method will accept the amount that needs to be added in balance
+        //Debit method will accept the amount that needs to be deducted from the balance.
+        //If amount is greater than balance raise an event → insufficient balance
+        //If balance is low (< 3000) raise an event low balance
+        //If balance is zero then raise an event zero balance
+
+        //event
+        public event DisplayMessage ZeroBalance;
+        public event DisplayMessage InsufficientBalance;
+        public event DisplayMessage LowBalance;
+        private double balance;
+        public Bank(double balance)
+        {
+            this.balance = balance;
+        }
+        public void Debit(double amount)
+        {
+            if(balance==0)
+            {
+                ZeroBalance();
+            }
+            else if (amount > balance)
+            {
+                InsufficientBalance();
+            }
+            else if(balance<3000)
+            {
+                LowBalance();
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine($"balance={balance}");
+            }
+        }
+
+      
+        public void Credit(double amount)
+        {
+            if(amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"Total Balance = {balance}");
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public delegate void DisplayMessage();
+    public class Program
+    {
+        static void ZeroBalanceMsg()
+        {
+            Console.WriteLine("You have zero balance");
+        }
+        static void InsufficientBalanceMsg()
+        {
+            Console.WriteLine("You have insufficient Balance");
+        }
+        static void LowBalanceMsg()
+        {
+            Console.WriteLine("You have low balance");
+        }
+        static void Main(string[] args)
+        {
+            Bank b = new Bank(0);
+            b.ZeroBalance += new DisplayMessage(ZeroBalanceMsg);
+            b.InsufficientBalance += new DisplayMessage(InsufficientBalanceMsg);
+            b.LowBalance += new DisplayMessage(LowBalanceMsg);
+
+            //b.Credit(500);
+            b.Debit(2000);
+        }
+    }
+}
